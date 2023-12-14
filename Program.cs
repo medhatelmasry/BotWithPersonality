d9bfab059c6d21e9bede4a0403145dec0fd6513f
medhatelmasry <medhatelmasry@live.com>
2023-12-14 14:28:16 -0800

    1st

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using Azure;
+using Azure.AI.OpenAI;
+using BotWithPersonality;
+
+string ENDPOINT = Utils.GetConfigValue("settings:endpoint");
+string KEY = Utils.GetConfigValue("settings:key");
+string DEPLOYMENT_NAME = Utils.GetConfigValue("settings:deployment-name");
+
+const string SYSTEM_MESSAGE
+    = """
+    You are a friendly assistant named DotNetBot. 
+    You prefer to use Canadian Newfoundland English as your language and are an expert in the .NET runtime 
+    and C# and F# programming languages.
+    Response using Newfoundland colloquialisms and slang.
+    """;
+
+var openAiClient = new OpenAIClient(
+    new Uri(ENDPOINT),
+    new AzureKeyCredential(KEY)
+);
+
+var chatCompletionsOptions = new ChatCompletionsOptions
+{
+    DeploymentName = DEPLOYMENT_NAME, // Use DeploymentName for "model" with non-Azure clients
+    Messages =
+    {
+        new ChatRequestSystemMessage(SYSTEM_MESSAGE),
+        new ChatRequestUserMessage("Introduce yourself"),
+    }
+};
+
+while (true)
+{
+    Console.WriteLine();
+    Console.Write("DotNetBot: ");
+
+    Response<ChatCompletions> chatCompletionsResponse = await openAiClient.GetChatCompletionsAsync(
+        chatCompletionsOptions
+    );
+
+    var chatMessage = chatCompletionsResponse.Value.Choices[0].Message;
+    Console.WriteLine($"[{chatMessage.Role.ToString().ToUpperInvariant()}]: {chatMessage.Content}");
+
+    chatCompletionsOptions.Messages.Add(new ChatRequestUserMessage(chatMessage.Content));
+
+    Console.WriteLine();
+
+    Console.Write("Enter a message: ");
+    var userMessage = Console.ReadLine();
+    chatCompletionsOptions.Messages.Add(new ChatRequestUserMessage(userMessage));
+}
+
---
+++ Utils.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace BotWithPersonality;
+
+public class Utils {
+    public static string GetConfigValue(string config) {
+
+        IConfigurationBuilder builder = new ConfigurationBuilder();
+
+        if (System.IO.File.Exists("appsettings.json"))
+            builder.AddJsonFile("appsettings.json", false, true);
+
+        if (System.IO.File.Exists("appsettings.Development.json"))
+            builder.AddJsonFile("appsettings.Development.json", false, true);
+
+        IConfigurationRoot root = builder.Build();
+
+        return root[config]!;
+    }
+}
+
